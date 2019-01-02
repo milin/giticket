b5d016b6fc95eeb9ea787c340ddbbcf74d699ead
Shakya, Milind <mshakya@singleplatform.com>
2019-01-02 15:57:10 -0500

    yappa

--- setup.py
+++ setup.py
@@ -48,11 +48,11 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/milin/giticket',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
     entry_points={
         'console_scripts': [
-            'gitticket = giticket.__main__:main',
+            'giticket = giticket.__main__:main',
         ]
 
     }
