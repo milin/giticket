9abec2e163c59abeb1787a9b91cbee7f2b2c0149
Shakya, Milind <mshakya@singleplatform.com>
2019-01-02 16:27:05 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/milin/giticket',
-    version='0.1.2',
+    version='0.1.4',
     zip_safe=False,
     entry_points={
         'console_scripts': [
