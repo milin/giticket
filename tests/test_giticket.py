a703234bec8ad891c52ca8e893f2624a2abfc2dd
Milind Shakya <milind.shakya@kraken.tech>
2026-05-15 12:04:37 -0400

    Add option to add tickets to trailer.

--- tests/test_giticket.py
+++ tests/test_giticket.py
@@ -182,6 +182,39 @@ def test_update_commit_message_capitalize_conventionalcommits(mock_branch_name, 
 
 
 @mock.patch(TESTING_MODULE + '.get_branch_name')
+def test_update_commit_message_to_trailer(mock_branch_name, tmpdir):
+    mock_branch_name.return_value = "JIRA-1234_new_feature"
+    path = tmpdir.join('file.txt')
+    path.write("Subject line\n\nBody text.")
+    update_commit_message(six.text_type(path), r'[A-Z]+-\d+',
+                          'regex_match', '{ticket}: {commit_msg}',
+                          to_trailer=True)
+    assert path.read() == "Subject line\n\nBody text.\n\nRefs: JIRA-1234\n"
+
+
+@mock.patch(TESTING_MODULE + '.get_branch_name')
+def test_update_commit_message_to_trailer_subject_only(mock_branch_name, tmpdir):
+    mock_branch_name.return_value = "JIRA-1234_new_feature"
+    path = tmpdir.join('file.txt')
+    path.write("Subject line")
+    update_commit_message(six.text_type(path), r'[A-Z]+-\d+',
+                          'regex_match', '{ticket}: {commit_msg}',
+                          to_trailer=True)
+    assert path.read() == "Subject line\n\nRefs: JIRA-1234\n"
+
+
+@mock.patch(TESTING_MODULE + '.get_branch_name')
+def test_update_commit_message_to_trailer_multiple_tickets(mock_branch_name, tmpdir):
+    mock_branch_name.return_value = "JIRA-1234-JIRA-5678"
+    path = tmpdir.join('file.txt')
+    path.write("Subject line")
+    update_commit_message(six.text_type(path), r'[A-Z]+-\d+',
+                          'regex_match', '{ticket}: {commit_msg}',
+                          to_trailer=True)
+    assert path.read() == "Subject line\n\nRefs: JIRA-1234, JIRA-5678\n"
+
+
+@mock.patch(TESTING_MODULE + '.get_branch_name')
 def test_update_commit_message_capitalize_disabled_by_default(mock_branch_name, tmpdir):
     mock_branch_name.return_value = "JIRA-1234_new_feature"
     path = tmpdir.join('file.txt')
@@ -246,10 +279,12 @@ def test_main(mock_update_commit_message, mock_argparse):
     mock_args.mode = 'underscore_split'
     mock_args.conventionalcommits = True
     mock_args.capitalize = 0
+    mock_args.to_trailer = False
     mock_argparse.ArgumentParser.return_value.parse_args.return_value = mock_args
     main()
     mock_update_commit_message.assert_called_once_with('foo.txt', r'[A-Z]+-\d+',
                                                        'underscore_split',
                                                        '{ticket} {commit_msg}',
                                                        True,
-                                                       0)
+                                                       0,
+                                                       False)
